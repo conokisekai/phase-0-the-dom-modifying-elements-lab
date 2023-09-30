1245849538c9c4997cd7b29b2b905c5f444745e0
Conrad Kambi <kambiconrad@gmail.com>
2023-09-30 11:03:57 +0300

    Completed assignmnet

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+// Remove the DOM node
+const main = document.querySelector('main');
+main.remove();
+
+// Create a newHeader
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+newHeader.innerHTML = "YOUR-NAME is the champion";
